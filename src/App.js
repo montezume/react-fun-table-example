8f2f1d7cd428848798ace3eed7fa3a97280b5e42
Malcolm Laing <Malcolm.Laing@adsdata.ca>
2017-11-23 19:09:08 -0500

    Add image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Table } from 'react-fun-table';
+import styled from 'styled-components';
 import './App.css';
 
 const data = [
@@ -83,6 +84,14 @@ const columns = [
  }
 ];
 
+const GithubImg = styled.img`
+	position: absolute;
+	top: 0;
+	right: 0;
+	border: 0;
+	z-index: 9999999;
+`;
+
 class App extends Component {
   // move sort logic?
 
@@ -91,6 +100,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="main-content">
+					<a href="https://github.com/montezume/react-fun-table"><GithubImg src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" /></a>
+
+
 					<h2>With one sticky column</h2>
           <Table
 						columns={columns}
